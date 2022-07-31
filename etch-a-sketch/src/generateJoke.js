0308db87071add23c4fe0d0af1076a0ca44a5e5b
Josh Sargento <jsargento477@gmail.com>
2022-08-01 00:39:59 +0800

    added webpack

---
+++ etch-a-sketch/src/generateJoke.js
@@ -0,0 +1,5 @@
+function generateJoke() {
+  return alert("I don't trust stairs. They're always up to something.")
+};
+
+export default generateJoke;
---
+++ etch-a-sketch/src/index.js
@@ -0,0 +1,6 @@
+import generateJoke from './generateJoke';
+import laughing from './assets/laughing.svg';
+
+const laughImg = document.getElementById('LaughImg');
+laughImg.src = laughing;
+generateJoke();
