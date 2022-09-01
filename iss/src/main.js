e39dbe993bcb02627c63324b3344d76e3675ab5c
Josh Sargento <jsargento477@gmail.com>
2022-09-02 05:15:57 +0800

    finished distance calculation logic

--- iss/src/main.js
+++ iss/src/main.js
@@ -5,7 +5,9 @@ import icon from './assets/myIcon.png';
 let firstLoad = true;
 let ISSCoords = [];
 let myCoords = [];
+
 const map = L.map('map').setView([0, 0], 1);
+const dist = document.getElementById('dist');
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '© OpenStreetMap',
@@ -17,10 +19,8 @@ const myIcon = L.icon({
 });
 
 function drawLine() {
-  const line = L.polyline([ISSCoords, myCoords]).addTo(map);
-  console.log(ISSCoords);
+  const pLine = L.polyline([ISSCoords, myCoords]).addTo(map);
   ISSCoords = [];
-  console.log(ISSCoords);
 }
 
 function getISS() {
@@ -28,16 +28,12 @@ function getISS() {
     .then((response) => response.json())
     .then((data) => {
       const { latitude, longitude } = data;
-      ISSCoords.push(latitude);
-      ISSCoords.push(longitude);
-      map.setView([latitude, longitude], 2);
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap',
-      }).addTo(map);
+      ISSCoords.push(parseFloat(latitude));
+      ISSCoords.push(parseFloat(longitude));
       const marker = L.marker([latitude, longitude], { icon: myIcon }).addTo(
         map
       );
+      writeDistance();
       drawLine();
     });
 }
@@ -46,8 +42,8 @@ function getmyLocation() {
   navigator.geolocation.getCurrentPosition((pos) => {
     const latitude = pos.coords.latitude;
     const longitude = pos.coords.longitude;
-    myCoords.push(latitude);
-    myCoords.push(longitude);
+    myCoords.push(parseFloat(latitude));
+    myCoords.push(parseFloat(longitude));
     const marker = L.marker([latitude, longitude]).addTo(map);
   });
 }
@@ -56,19 +52,41 @@ function getDistFromISS() {
   if (firstLoad === true) {
     getmyLocation();
     getISS();
+
     firstLoad = false;
   } else {
     getISS();
   }
 }
+function removeLayer() {
+  map.eachLayer((layer) => {
+    if (layer._leaflet_id != 27) {
+      map.removeLayer(layer);
+    }
+  });
+}
 
+function writeDistance() {
+  let distance = calcCrow(myCoords[0], myCoords[1], ISSCoords[0], ISSCoords[1]);
+  console.log(distance);
+  console.log(myCoords[0]);
+  console.log(myCoords[1]);
+  console.log(ISSCoords[0]);
+  console.log(ISSCoords[1]);
+  dist.innerText = calcCrow(
+    myCoords[0],
+    myCoords[1],
+    ISSCoords[0],
+    ISSCoords[1]
+  ).toFixed(1);
+}
 //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
 function calcCrow(myLat, myLon, ISSLat, ISSLon) {
   let R = 6371; // km
   let dLat = toRad(ISSLat - myLat);
   let dLon = toRad(ISSLon - myLon);
   let lat1 = toRad(myLat);
-  let lat2 = toRad(myLon);
+  let lat2 = toRad(ISSLat);
 
   let a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
